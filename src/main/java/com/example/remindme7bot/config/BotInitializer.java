6fd1b3a0ae2f65c14c655f1e26da5c35bb523e00
BadHard <BadHard101@yandex.ru>
2023-09-13 08:51:19 +0300

    logs added

--- src/main/java/com/example/remindme7bot/config/BotInitializer.java
+++ src/main/java/com/example/remindme7bot/config/BotInitializer.java
@@ -1,6 +1,7 @@
 package com.example.remindme7bot.config;
 
 import com.example.remindme7bot.service.TelegramBot;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
@@ -9,6 +10,7 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+@Slf4j
 @Component
 public class BotInitializer {
 
@@ -25,9 +27,8 @@ public class BotInitializer {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         try {
             telegramBotsApi.registerBot(bot);
-        }
-        catch (TelegramApiException e) {
-
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
         }
     }
 }
--- src/main/java/com/example/remindme7bot/service/TelegramBot.java
+++ src/main/java/com/example/remindme7bot/service/TelegramBot.java
@@ -1,12 +1,14 @@
 package com.example.remindme7bot.service;
 
 import com.example.remindme7bot.config.BotConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+@Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
@@ -45,6 +47,7 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     private void startCommandReceived(long chatId, String name) {
         String answer = "Привет, " + name + "!\nДобро пожаловать!";
+        log.info("Replied to user " + name);
         sendMessage(chatId, answer);
     }
 
@@ -57,7 +60,7 @@ public class TelegramBot extends TelegramLongPollingBot {
             execute(message);
         }
         catch (TelegramApiException e) {
-
+            log.error("Error occurred: " + e.getMessage());
         }
     }
 }
